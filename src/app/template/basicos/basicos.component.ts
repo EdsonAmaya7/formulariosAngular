77bd71928c2357f6473cdaa1d0b67a06942be410
EdsonAmaya7 <edson_amaya99@live.com>
2023-02-13 11:57:08 -0600

    formularios por aproximacion template

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+
+export class BasicosComponent {
+
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  initForm = {
+    producto: '',
+    precio: 0,
+    existencias: 0
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  nombreValido(): boolean {
+    return this.miFormulario?.controls["producto"]?.invalid && this.miFormulario?.controls["producto"]?.touched
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls["precio"]?.touched && this.miFormulario?.controls["precio"]?.value < 0
+  }
+
+  // guardar(miFormulario: NgForm){
+  guardar() {
+    console.log(this.miFormulario.value)
+
+    this.miFormulario.reset(this.initForm)
   }
 
 }
--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+export class SwitchesComponent {
 
-  ngOnInit(): void {
+  persona = {
+    genero : 'F',
+    notificaciones: true,
   }
 
+  terminosYCondiciones: boolean = false;
 }
