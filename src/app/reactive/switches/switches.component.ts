29dee689246ae3efc3c9849f1723e79ea00384a3
EdsonAmaya7 <edson_amaya99@live.com>
2023-02-13 20:04:29 -0600

    formularios reactivos

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControlDirective, FormGroup, Validators } from '@angular/forms';
+import { CustomMinDirective } from '../../template/directives/custom-min.directive';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({ ...this.persona, condiciones: true });
+
+    // subscribirse a los cambios del formulario
+    // this.miFormulario.valueChanges.subscribe(form =>{
+    //   console.log(form)
+    // })
+    // subscribirse a los cambios del formulario de un campo
+    this.miFormulario.get('condiciones')?.valueChanges.subscribe(newValue => {
+      console.log('newValue', newValue)
+    })
+
+    // subscribirse a los cambios del formulario
+    // extrae condiciones como una variable independiente
+    // y guarda el rest
+    this.miFormulario.valueChanges.subscribe(({ condiciones, ...rest }) => {
+      console.log('rest', rest)
+      this.persona = rest
+    })
+  }
+
+  // sincronizar el objeto formulario con el de la persona al precionar guardar
+  guardar() {
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.condiciones
+    this.persona = formValue
+    console.log(formValue)
   }
 
 }
